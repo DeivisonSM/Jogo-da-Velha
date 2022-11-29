adb3c12d7d7e3db9379617b2975a27e7872a6be1
DeivisonSM <deivisonsm.dev@gmail.com>
2022-11-28 22:10:30 -0300

    building logic and interface

--- scripts/game.js
+++ scripts/game.js
@@ -1,45 +1,23 @@
 const game = {
     players: [
         {
-            name: "Deivison",
-            type: "player",
-            icon: 0,
+            name: "",
+            type: "",
+            icon: "",
             score: 0
         },
         {
-            name: "João",
-            type: "player",
-            icon: 1,
+            name: "",
+            type: "",
+            icon: "",
             score: 0
         }
     ],
 
-    board: ["","","","","","","","",""],
-
-    winnerPositions: [
-        [0,1,2],
-        [3,4,5],
-        [6,7,8],
-        [0,3,6],
-        [1,4,7],
-        [2,5,8],
-        [0,4,8],
-        [2,4,6]
-    ],
-
-    firstPlayerTurn: 0,
-
-    playerTurn: 0,
-
-    gameOver: false,
-
     ready: false,
 
-    endRoundInfo: {
-        name: "",
-        winnerPositions: [],
-        tie: false,
-    }
+    playerTurn: 0
+
 };
 
 function setPlayersType(gameMode){
@@ -59,6 +37,14 @@ function setPlayersType(gameMode){
     };
 };
 
+function setPlayerIcon(icon, player){
+    const anotherIcon = icon === 0 ? 1 : 0;
+    const anotherPlayer = player === 0 ? 1 : 0;
+
+    game.players[player].icon = icon;
+    game.players[anotherPlayer].icon = anotherIcon;
+};  
+
 function setPlayerName(playerName, player){
     playerName = playerName.replace(/\s{2,}/g, ' ').trim();
     
@@ -68,76 +54,6 @@ function setPlayerName(playerName, player){
         game.players[player].name = `Jogador ${player + 1}`;  
 };
 
-function setPlayerIcon(icon, player){
-    let anotherIcon = icon === 0 ? 1 : 0;
-    let anotherPlayer = player === 0 ? 1 : 0;
-
-    game.players[player].icon = icon;
-    game.players[anotherPlayer].icon = anotherIcon;
-};  
-
 function setGameReady(){
     game.ready = true;
 }
-
-function verifyWinner(){
-    game.winnerPositions.forEach((positions)=>{
-       if(game.board[positions[0]] === game.players[game.playerTurn].icon &&
-       game.board[positions[1]] === game.players[game.playerTurn].icon &&
-       game.board[positions[2]] === game.players[game.playerTurn].icon){
-            game.gameOver = true;
-            game.players[game.playerTurn].score++;
-            game.endRoundInfo.name = game.players[game.playerTurn].name;
-            game.endRoundInfo.winnerPositions = [positions[0], positions[1], positions[2]];
-            return;
-       };
-    });
-};
-
-function verifyTie(){
-    game.gameOver = true;
-    game.endRoundInfo.tie = true;
-
-    game.board.forEach((position)=>{
-        if(position === ""){
-            game.gameOver = false;
-            game.endRoundInfo.tie = false;
-            return;
-        };
-    });
-};
-
-function changePlayerTurn(){
-    game.playerTurn = game.playerTurn === 0 ? 1 : 0;
-};
-
-function canHandleMove(cell){    
-    if(game.board[cell] === "" && !game.gameOver){
-        handleMove(cell);
-        return true;
-    }else{
-        return false;
-    };
-};
-
-function handleMove(cell){
-    game.board[cell] = game.players[game.playerTurn].icon;
-    verifyWinner();
-    
-    if(!game.gameOver)
-        verifyTie();
-  
-};
-
-function computerMove(){
-    let aux = false;
-    let randomMove;
-
-    while(!aux){
-        randomMove = Math.floor(Math.random() * game.board.length);
-        if(game.board[randomMove] === ""){
-            aux = true;
-            return randomMove;
-        };
-    };
-};
---
+++ scripts/interface.js
@@ -0,0 +1,154 @@
+const gameModeButtons = document.querySelectorAll(".menu__button");
+const startGameButton = document.querySelector("#start-game-button");
+
+const inputsPlayerName = document.querySelectorAll(".settings__player-name");
+const buttonsPlayerIcon = document.querySelectorAll(".settings__player-icons");
+const allIconButtons = document.querySelectorAll(".settings__icon-button");
+
+const boardCells = document.querySelectorAll(".board__cell");
+
+let currentContainer = 0;
+
+function changeSection(){
+    const pageContainers = document.querySelectorAll(".container__single");
+    
+    let previousContainer = currentContainer - 1 < 0 ? pageContainers.length - 1 : currentContainer - 1;
+    let nextContainer = currentContainer + 1 >= pageContainers.length ? 0 : currentContainer + 1;
+    
+    pageContainers[previousContainer].classList.remove("container__single--waiting");
+
+    pageContainers[currentContainer].classList.remove("container__single--active");
+    pageContainers[currentContainer].classList.add("container__single--waiting");
+
+    pageContainers[nextContainer].classList.add("container__single--active");
+
+    currentContainer = nextContainer;
+};
+
+function attButtonsIconStyle(){
+    allIconButtons.forEach((button)=>{
+        button.classList.remove("settings__icon-button--active");
+    });
+
+    if(game.ready){
+        const iconButtonPlayer1 = buttonsPlayerIcon[0].children[game.players[0].icon];
+        const iconButtonPlayer2 = buttonsPlayerIcon[1].children[game.players[1].icon];
+
+        iconButtonPlayer1.classList.add("settings__icon-button--active");
+        iconButtonPlayer2.classList.add("settings__icon-button--active");
+    };
+};
+
+function attStartGameButtonStyle(){
+    if(game.ready)
+        startGameButton.classList.add("settings__button--active");
+    else
+        startGameButton.classList.remove("settings__button--active");
+};
+
+function getPlayersNames(){
+    inputsPlayerName.forEach((input, index)=>{
+        setPlayerName(input.value, index);
+    });
+};
+
+function attScoreboard(){
+    const player1Name = document.querySelector("#scoreboard-player1 > .game__player-name");
+    const player2Name = document.querySelector("#scoreboard-player2 > .game__player-name");
+    const player1Score = document.querySelector("#scoreboard-player1 > .game__player-score");
+    const player2Score = document.querySelector("#scoreboard-player2 > .game__player-score");
+
+    function attScoreboardStyle(){
+        player1Name.style.color = `var(--icon-${game.players[0].icon}-color)`;
+        player2Name.style.color = `var(--icon-${game.players[1].icon}-color)`;
+    };
+
+    player1Name.innerText = game.players[0].name;
+    player2Name.innerText = game.players[1].name;
+    player1Score.innerText = game.players[0].score;
+    player2Score.innerText = game.players[1].score;
+
+    attScoreboardStyle();
+};
+
+function attPlayerTurnStyle(){
+    const scoreboardsPlayers = document.querySelectorAll(".game__scoreboard-player");
+
+    scoreboardsPlayers.forEach((scoreboard)=>{
+        scoreboard.classList.remove("game__scoreboard-player--active");
+    });
+
+    scoreboardsPlayers[game.playerTurn].classList.add("game__scoreboard-player--active");
+};
+
+function startGame(){
+
+
+
+
+
+
+
+    boardCells.forEach((cell)=>{
+        cell.removeEventListener("click", handleClickPlayer);
+    });
+
+    verifyPlayerType();
+
+};
+
+gameModeButtons.forEach((button, index)=>{
+    button.addEventListener("click",()=>{
+        setPlayersType(index);
+        changeSection();
+    });
+});
+
+buttonsPlayerIcon.forEach((buttons, index)=>{
+    buttons = [...buttons.children];
+
+    buttons.forEach((button, icon)=>{
+        button.addEventListener("click",()=>{
+            setPlayerIcon(icon, index);
+            setGameReady();
+            attButtonsIconStyle();
+            attStartGameButtonStyle();
+        });
+    });
+});
+
+startGameButton.addEventListener("click", ()=>{
+    if(game.ready){
+        getPlayersNames();
+        changeSection();
+        attScoreboard();
+        attPlayerTurnStyle();
+        startGame();
+    }else{
+        allIconButtons.forEach((button)=>{
+            button.classList.remove("settings__icon-button--error");
+
+            setTimeout(()=>{
+                button.classList.add("settings__icon-button--error");
+            }, 5);
+        });
+    };
+});
+
+function resetGame(){
+    game.players.forEach((player)=>{player.score = 0});
+    game.ready = false;
+    game.playerTurn = 0;
+};
+
+function resetInterface(){
+    attButtonsIconStyle();
+    attStartGameButtonStyle();
+    inputsPlayerName.forEach((input)=>{
+        input.value = "";
+    });
+};
+
+function resetBoard(){
+
+};
